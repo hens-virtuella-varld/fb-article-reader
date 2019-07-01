da5c8a098f7c9251139b3601162c8e102c6038d5
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2019-07-01 15:15:55 +0800

    process all csv in folder, change extracted columns

--- process-csv.py
+++ process-csv.py
@@ -1,8 +1,8 @@
-import csv
+from os import listdir
 import pandas as pd
 
-
-df = pd.read_csv('.csv')
-
-saved_column = df[['date', 'text', 'post_id']]
-timeline_articles = saved_column.sort_values('date')
+for csvfile in listdir('csv'):
+    df = pd.read_csv('csv/'+csvfile)
+    saved_column = df[['date', 'text', 'url', 'source']]
+    timeline_articles = saved_column.sort_values('date')
+    print(timeline_articles.head())
