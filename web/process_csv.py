400f629e79415c48527ee5e3cd68ce49312bf948
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2019-07-08 13:10:40 +0800

    Edit reader template, edit index template, add detail template

--- web/process_csv.py
+++ web/process_csv.py
@@ -2,9 +2,8 @@ from os import listdir
 import pandas as pd
 
 
-def process_csv():
-    for csvfile in listdir('csv'):
-        df = pd.read_csv('csv/'+csvfile)
-        saved_column = df[['date', 'text', 'url', 'source']]
-        timeline_articles = saved_column.sort_values('date').to_dict('records')
-        return timeline_articles
+def process_csv(page):
+    df = pd.read_csv('csv/'+page)
+    saved_column = df[['date', 'text', 'url', 'source']]
+    timeline_articles = saved_column.sort_values('date').to_dict('records')
+    return timeline_articles
--- web/reader/views.py
+++ web/reader/views.py
@@ -1,8 +1,14 @@
+from os import listdir
 from django.shortcuts import render
 
 from process_csv import process_csv
 
 
 def index(request):
-    context = {'timeline_articles': process_csv()}
+    context = {'pages': listdir('csv')}
     return render(request, 'reader/index.html', context)
+
+
+def detail(request, page):
+    context = {'timeline_articles': process_csv(page)}
+    return render(request, 'reader/detail.html', context)
