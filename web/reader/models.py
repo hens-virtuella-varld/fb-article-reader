a3acbb45b8208acb9d7d48471ce28d958cf7452a
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2019-07-09 09:13:56 +0800

    Add FanPage and Article models

--- web/reader/models.py
+++ web/reader/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class FanPage(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Article(models.Model):
+    fanpage = models.ForeignKey(FanPage, on_delete=models.CASCADE)
+    text = models.TextField()
+    time = models.DateTimeField()
+    url = models.URLField(max_length=2000)
+    read = models.BooleanField(default=False)
+    starred = models.BooleanField(default=False)
