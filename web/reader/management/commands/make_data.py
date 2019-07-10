49ee86336fbbb0d0c520d78f66d5990e31516bd8
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2019-07-10 10:14:33 +0800

    Load article into database in make_data script

--- web/reader/management/commands/make_data.py
+++ web/reader/management/commands/make_data.py
@@ -1,10 +1,10 @@
-from django.core.management.base import BaseCommand, CommandError
-
-from reader.models import FanPage
-
+import csv
 import urllib.request
 from bs4 import BeautifulSoup
-import csv
+
+from django.core.management.base import BaseCommand, CommandError
+from reader.models import FanPage, Article
+from process_csv import process_csv
 
 
 class Command(BaseCommand):
@@ -15,4 +15,13 @@ class Command(BaseCommand):
                 soup = BeautifulSoup(urllib.request.urlopen(
                     "https://mbasic.facebook.com/" + row[0]), "lxml")
                 fanpage_name = soup.title.text
-                FanPage.objects.create(name=fanpage_name)
+                fanpage = FanPage.objects.create(name=fanpage_name)
+
+                timeline_articles = process_csv(row[0]+'.csv')
+                fanpage.article_set.bulk_create([
+                    Article(
+                        time=article['date'],
+                        url=article['url'],
+                        text=article['text']
+                    ) for article in timeline_articles
+                ])
--- web/reader/models.py
+++ web/reader/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class FanPage(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     fanpage = models.ForeignKey(FanPage, on_delete=models.CASCADE)
@@ -12,3 +15,6 @@ class Article(models.Model):
     url = models.URLField(max_length=2000)
     read = models.BooleanField(default=False)
     starred = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.fanpage}, {self.time}'
